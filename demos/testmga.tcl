09ee607335bd165d76a6a1efa9c3795fbdc03132
peter@arch1701 <p.dean@internode.on.net>
2024-02-22 08:27:58 +1000

    datum shift demo

--- demos/testmga.tcl
+++ demos/testmga.tcl
@@ -3,11 +3,12 @@ package require proj
 
 set pos [list 502810 6964520 0]
 puts $pos
+
 set cs [proj crs2crs EPSG:28356 EPSG:4283]
 puts $cs
 set result [proj fwd $cs $pos]
 puts [format "%.9f %.9f %.3f" {*}$result]
-# output -> -27.44279 153.02843 41.901
+# output -> -27.44279 153.02843 0.0
 set result2 [proj inv $cs $result]
 puts [format "%.5f %.5f %.3f" {*}$result2]
 
@@ -15,6 +16,13 @@ set cs [proj crs2crs EPSG:7856 EPSG:7844]
 puts $cs
 set result [proj fwd $cs $pos]
 puts [format "%.9f %.9f %.3f" {*}$result]
-# output -> -27.44279 153.02843 41.901
+# output -> -27.44279 153.02843 0.0
 set result2 [proj inv $cs $result]
 puts [format "%.5f %.5f %.3f" {*}$result2]
+
+set cs [proj crs2crs EPSG:28356 EPSG:7856]
+puts $cs
+set result [proj fwd $cs $pos]
+puts [format "%.3f %.3f %.3f" {*}$result]
+set result2 [proj inv $cs $result]
+puts [format "%.3f %.3f %.3f" {*}$result2]
