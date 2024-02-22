36cca5a6f7837a60788ebb0f4327bcb21c532c46
peter dean <p.dean@internode.on.net>
2024-02-22 12:58:49 +1000

    tests

--- demos/testmga.tcl
+++ demos/testmga.tcl
@@ -1,12 +1,13 @@
 #!/usr/bin/env tclsh
 package require proj
 
+set zone 56
 set pos [list 502810 6964520 0]
 puts $pos
 
 puts ""
 
-set cs [proj crs2crs EPSG:28356 EPSG:4283]
+set cs [proj crs2crs EPSG:283$zone EPSG:4283]
 set result [proj fwd $cs $pos]
 puts [format "%.9f %.9f %.3f" {*}$result]
 set result2 [proj inv $cs $result]
@@ -14,7 +15,7 @@ puts [format "%.5f %.5f %.3f" {*}$result2]
 
 puts ""
 
-set cs [proj crs2crs EPSG:7856 EPSG:7844]
+set cs [proj crs2crs EPSG:78$zone EPSG:7844]
 set cs [proj norm $cs]
 set result [proj fwd $cs $pos]
 puts [format "%.9f %.9f %.3f" {*}$result]
@@ -23,7 +24,7 @@ puts [format "%.5f %.5f %.3f" {*}$result2]
 
 puts ""
 
-set cs [proj crs2crs EPSG:28356 EPSG:7856]
+set cs [proj crs2crs EPSG:283$zone EPSG:78$zone]
 set result [proj fwd $cs $pos]
 puts [format "%.3f %.3f %.3f" {*}$result]
 set result2 [proj inv $cs $result]
@@ -31,15 +32,34 @@ puts [format "%.3f %.3f %.3f" {*}$result2]
 
 puts ""
 
-set mga2gda [proj crs2crs epsg:28356 epsg:4283]
+set mga2gda [proj crs2crs epsg:283$zone epsg:4283]
 set gda2ahd [proj crs2crs epsg:4939 epsg:9464]
 set result [proj inv $mga2gda [proj inv $gda2ahd [proj fwd $mga2gda $pos]]]
 puts [format "%.3f %.3f %.3f" {*}$result]
 
 puts ""
 
-set mga2gda [proj crs2crs epsg:7856 epsg:7844]
+set mga2gda [proj crs2crs epsg:78$zone epsg:7844]
 set gda2ahd [proj crs2crs epsg:7843 epsg:9463]
 set result [proj inv $mga2gda [proj inv $gda2ahd [proj fwd $mga2gda $pos]]]
 puts [format "%.3f %.3f %.3f" {*}$result]
 
+puts ""
+
+set S    "+proj=pipeline  +zone=$zone +south +ellps=GRS80"
+append S " +step +inv +proj=utm"
+append S " +step +proj=vgridshift +grids=au_ga_AUSGeoid09_V1.01.tif"
+append S " +step +proj=utm"
+set cs [proj create $S]
+set result [proj inv $cs $pos]
+puts [format "%.3f %.3f %.3f" {*}$result]
+
+puts ""
+
+set S    "+proj=pipeline  +zone=$zone +south +ellps=GRS80"
+append S " +step +inv +proj=utm"
+append S " +step +proj=vgridshift +grids=au_ga_AUSGeoid2020_20180201.tif"
+append S " +step +proj=utm"
+set cs [proj create $S]
+set result [proj inv $cs $pos]
+puts [format "%.3f %.3f %.3f" {*}$result]
