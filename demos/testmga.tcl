0604db0a03f2388daf00aa73422e39ce5f53f337
peter@arch1701 <p.dean@internode.on.net>
2024-02-22 12:00:51 +1000

    tests various

--- demos/testmga.tcl
+++ demos/testmga.tcl
@@ -9,7 +9,6 @@ puts ""
 set cs [proj crs2crs EPSG:28356 EPSG:4283]
 set result [proj fwd $cs $pos]
 puts [format "%.9f %.9f %.3f" {*}$result]
-# output -> -27.44279 153.02843 0.0
 set result2 [proj inv $cs $result]
 puts [format "%.5f %.5f %.3f" {*}$result2]
 
@@ -19,7 +18,6 @@ set cs [proj crs2crs EPSG:7856 EPSG:7844]
 set cs [proj norm $cs]
 set result [proj fwd $cs $pos]
 puts [format "%.9f %.9f %.3f" {*}$result]
-# output -> -27.44279 153.02843 0.0
 set result2 [proj inv $cs $result]
 puts [format "%.5f %.5f %.3f" {*}$result2]
 
